1c57a46a14e9143148130b5c6177549f53d031b8
leosiu wish <55885280+burnxwallpaper@users.noreply.github.com>
2020-02-24 21:59:34 +0800

    restore cors setting

--- src/serverAPI/server.js
+++ src/serverAPI/server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
-/*app.use((req, res, next) => {
+app.use((req, res, next) => {
     res.set({
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "*",
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
         //"Content-Type": "text/html"
     })
     next()
-})*/
+})
 
 app.use(cors())
 
